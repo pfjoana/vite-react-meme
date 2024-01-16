e543a001681ed6974eb4365370c1392cd4ff8a9e
joana faria <paisdefaria.joana@gmail.com>
2024-01-16 15:34:10 +0000

    created and styled header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
+import Header from './components/Header.jsx'
 
-// function App() {
-//   return (
-//   )
-// }
+function App() {
+  return (
+    <>
+      <Header/>
 
-// export default App
+    </>
+  )
+}
+
+export default App
